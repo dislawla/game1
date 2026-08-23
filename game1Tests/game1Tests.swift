23d3429bdd6c7aebf0b285bf9c93ef5fde417ba7
dislawla <dislawla@yandex.ru>
2026-08-23 20:01:35 +0300

    Initial Commit

---
+++ game1Tests/game1Tests.swift
@@ -0,0 +1,19 @@
+//
+//  game1Tests.swift
+//  game1Tests
+//
+//  Created by Dislawla on 23.08.2026.
+//
+
+import Testing
+@testable import game1
+
+struct game1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
